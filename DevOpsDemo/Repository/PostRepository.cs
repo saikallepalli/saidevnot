8a23d7134aa6b0e38bb27a180c6643ee1d8d5d00
saikallepalli <48295199+saikallepalli@users.noreply.github.com>
2021-11-23 20:19:44 +0530

    Update PostRepository.cs
    commit name

--- DevOpsDemo/Repository/PostRepository.cs
+++ DevOpsDemo/Repository/PostRepository.cs
@@ -11,9 +11,9 @@ namespace DevOpsDemo.Repository
         public List<PostViewModel> GetPosts()
         {
             var posts = new List<PostViewModel> {
-                new PostViewModel(){ PostId =101, Title = "DevOps Demo Title 1", Description ="DevOps Demo Description 1", Author="Mukesh Kumar"},
-                new PostViewModel(){ PostId =102, Title = "DevOps Demo Title 2", Description ="DevOps Demo Description 2", Author="Banky Chamber"},
-                new PostViewModel(){ PostId =103, Title = "DevOps Demo Title 3", Description ="DevOps Demo Description 3", Author="Rahul Rathor"},
+                new PostViewModel(){ PostId =101, Title = "DevOps Demo Title 1", Description ="DevOps Demo Description 1", Author="sai"},
+                new PostViewModel(){ PostId =102, Title = "DevOps Demo Title 2", Description ="DevOps Demo Description 2", Author="kumar"},
+                new PostViewModel(){ PostId =103, Title = "DevOps Demo Title 3", Description ="DevOps Demo Description 3", Author="kalleaplli"},
             };
 
             return posts;
